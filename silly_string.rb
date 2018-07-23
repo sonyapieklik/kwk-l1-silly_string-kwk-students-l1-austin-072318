237394e9fc55d041a98e2eed308604c1199d9f9f
sonyapieklik <piesonya@gmail.com>
2018-07-23 17:44:39 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -46,6 +46,6 @@ sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples 
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence.gsub  # Add your gsub chain here
+sentence = sentence.gsub(/[AO]/) # Add your gsub chain here
 
 puts sentence
